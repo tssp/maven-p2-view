cb719767213795fa6609d73a2cd01cadd502cbc2
tssp <tim@coding-me.com>
2015-08-03 10:04:09 +0200

    added analyzer base response

--- core/src/main/scala/io/coding/me/m2p2/core/actor/artifact/ArtifactAnalyzer.scala
+++ core/src/main/scala/io/coding/me/m2p2/core/actor/artifact/ArtifactAnalyzer.scala
@@ -12,7 +12,9 @@ import akka.actor.ActorLogging
 import akka.actor.Props
 import akka.actor.actorRef2Scala
 import io.coding.me.m2p2.core.actor.RepositoryId
+import io.coding.me.m2p2.core.analyzer.P2Artifact.P2Artifact
 import io.coding.me.m2p2.core.analyzer.P2Feature
+import io.coding.me.m2p2.core.analyzer.P2Metadata.P2Unit
 import io.coding.me.m2p2.core.internal.metric.FileAnalyzerMetrics
 import io.coding.me.m2p2.core.internal.metric.convert2extension
 
@@ -30,6 +32,8 @@ object ArtifactAnalyzer {
     def result: Option[T]
   }
 
+  case class P2MetadataAnalyzeResponse(val file: File, val result: Option[P2Unit]) extends AnalyzeResponse[P2Unit]
+  case class P2ArtifactAnalyzeResponse(val file: File, val result: Option[P2Artifact]) extends AnalyzeResponse[P2Artifact]
   case class JarAnalyzeResponse(val file: File, val result: Option[P2Feature]) extends AnalyzeResponse[P2Feature]
 
   /**
@@ -37,8 +41,22 @@ object ArtifactAnalyzer {
    */
   def props[T](repositoryId: RepositoryId, name: String, analyzer: File => Try[Option[T]], factory: (File, Option[T]) => AnalyzeResponse[T]): Props = Props(new ArtifactAnalyzer(repositoryId, name, analyzer, factory))
 
+  /**
+   * Analyzer for JAR files containing feature.xml files
+   */
   def jarProps(repositoryId: RepositoryId) = props[P2Feature](repositoryId, "jar-file", null, (file, result) => JarAnalyzeResponse(file, result))
+  
+  /**
+   * Analyzer for *.p2metadata.xml files containing P2Unit artifacts
+   */
+  def p2metadataProps(repositoryId: RepositoryId) = props[P2Unit](repositoryId, "p2metadata-file", null, (file, result) => P2MetadataAnalyzeResponse(file, result))
 
+  /**
+   * Analyzer for *.p2artifacts.xml files containing P2Artifact artifacts
+   */
+  def p2artifactProps(repositoryId: RepositoryId) = props[P2Artifact](repositoryId, "p2artifact-file", null, (file, result) => P2ArtifactAnalyzeResponse(file, result))
+
+  
 }
 
 class ArtifactAnalyzer[T](repositoryId: RepositoryId, name: String, analyzer: File => Try[Option[T]], factory: (File, Option[T]) => ArtifactAnalyzer.AnalyzeResponse[T]) extends Actor with ActorLogging {
